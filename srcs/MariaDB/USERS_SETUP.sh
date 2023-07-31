0ee15465a79bc3086f3abeaffb45094200e8f7a2
aelaoufi <aelaoufi@student.1337.ma>
2023-07-31 17:16:27 +0100

    mariadb is working

--- srcs/MariaDB/USERS_SETUP.sh
+++ srcs/MariaDB/USERS_SETUP.sh
@@ -1,8 +1,12 @@
+service mysql start
+
+mysql -u root -e "CREATE DATABASE zab;"
+mysql -u root -e "CREATE USER 'USER'@'%' IDENTIFIED BY 'anassraja';"
+mysql -u root -e "REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "CREATE USER 'USER'@'%' IDENTIFIED BY 'anass';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY 'anassraja';"
+mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY 'anassraja';" 
 
 exec $@
