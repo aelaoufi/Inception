44185e0a7edba72b839a2d4e446bdfbefa8f3d9f
aelaoufi <aelaoufi@student.1337.ma>
2023-08-08 17:58:14 +0100

    starting docker compose

--- srcs/requirements/Wordpress/Wp_script.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p var/www/html/wordpress
-
-cd /var/www/html/wordpress
-
-wp core download --allow-root
-#wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
-#wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a --allow-root
-
-exec $@
