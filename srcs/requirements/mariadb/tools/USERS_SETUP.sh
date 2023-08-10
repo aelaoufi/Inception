32d942a8431f684f1d4c683624b54f98f956b93e
aelaoufi <aelaoufi@student.1337.ma>
2023-08-10 21:52:10 +0100

    working, still need more testing

--- srcs/requirements/mariadb/tools/USERS_SETUP.sh
+++ srcs/requirements/mariadb/tools/USERS_SETUP.sh
@@ -2,11 +2,10 @@ service mysql start
 
 mysql -u root -e "CREATE DATABASE mdb;"
 mysql -u root -e "CREATE USER 'USER'@'%' IDENTIFIED BY 'anassraja';"
-mysql -u root -e "REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY 'anassraja';" 
+mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY 'anassanass';" 
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 exec $@
