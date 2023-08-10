62244155810f8efa7d91110de48bce8c4e4b7ccf
aelaoufi <aelaoufi@student.1337.ma>
2023-08-10 01:14:14 +0100

    working on yml file, database problem :///

--- srcs/requirements/mariadb/tools/USERS_SETUP.sh
+++ srcs/requirements/mariadb/tools/USERS_SETUP.sh
@@ -1,6 +1,6 @@
 service mysql start
 
-mysql -u root -e "CREATE DATABASE zab;"
+mysql -u root -e "CREATE DATABASE mdb;"
 mysql -u root -e "CREATE USER 'USER'@'%' IDENTIFIED BY 'anassraja';"
 mysql -u root -e "REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
